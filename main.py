9c8073832a69c9c3bd0942303ce0af7fe4200f6c
lsimoesdeoliveira <lucas.fowl@gmail.com>
2017-06-15 16:36:28 -0300

    defining functions
    splitting the code into functions for better readability

--- main.py
+++ main.py
@@ -34,38 +34,47 @@ arquivo = menu[FILE_MENU][0]
 complement = menu[FILE_MENU][1]
 url = 'http://www.ons.org.br/resultados_operacao/SDRO/Diario/'
 
-# Abre o histórico de geração e procura a última data com dados de medição
-with open(arquivo, newline='') as csvFile:
-    csvReader = csv.reader(csvFile, delimiter=';')
-    lastDate = list(csvReader)[-1][-0]
-    lastDate = datetime.datetime.strptime(lastDate, "%d/%m/%Y")
-    d1 = lastDate.date()
-csvFile.close()
 
-# Procura no site do ONS as medições mais recentes
-reqDate = requests.get('http://www.ons.org.br/resultados_operacao/SDRO/Diario/topo.htm')
-reqDate.encoding = 'utf8'
-soupDate = bs4.BeautifulSoup(reqDate.text, "html.parser")
-textDate = soupDate.find_all('option')
-textDate = textDate[1].get_text()
-textDate = datetime.datetime.strptime(textDate[-10:], '%d/%m/%Y')
-d2 = textDate.date()
+def find_last_date_csv(path_to_file):
+    # Abre o histórico de geração e procura a última data com dados de medição
+    with open(path_to_file, newline='') as csvFile:
+        csvReader = csv.reader(csvFile, delimiter=';')
+        lastDate = list(csvReader)[-1][-0]
+        lastDate = datetime.datetime.strptime(lastDate, "%d/%m/%Y")
+    csvFile.close()
+    return lastDate.date()
+
+
+def find_last_date_ons_site():
+    # Procura no site do ONS as medições mais recentes
+    reqDate = requests.get('http://www.ons.org.br/resultados_operacao/SDRO/Diario/topo.htm')
+    reqDate.encoding = 'utf8'
+    soupDate = bs4.BeautifulSoup(reqDate.text, "html.parser")
+    textDate = soupDate.find_all('option')
+    textDate = textDate[1].get_text()
+    textDate = datetime.datetime.strptime(textDate[-10:], '%d/%m/%Y')
+    return textDate.date()
+
+
+def generate_dates_url(d1, numDays):
+    # Gerando datas para a url
+    dates = []
+    for i in range(1, numDays + 1):
+        dates.append((d1 + datetime.timedelta(days=i)).strftime("%Y_%m_%d"))
+    return dates
+
 
 # Entrada de datas específicas para testes
 # d1 = datetime.date(2017, 5, 16)
 # d2 = datetime.date(2017, 5, 31)
-
-# Número de dias entre o último registro local e o disponível no ONS
-numDays = (d2 - d1).days
-dates = []
+d1 = find_last_date_csv(arquivo)
+d2 = find_last_date_ons_site()
 
 print('A última data de medições no arquivo local é: {}'.format(d1))
 print('A última data de medições no site do ONS é: {}'.format(d2))
 
-
-# Gerando datas para a url
-for i in range(1, numDays + 1):
-    dates.append((d1 + datetime.timedelta(days=i)).strftime("%Y_%m_%d"))
+numDays = (d2 - d1).days
+dates = generate_dates_url(d1, numDays)
 
 if (input('Importar útimas medições? (S/N) ').upper() == ('S')) and (numDays > 0):
 
@@ -75,6 +84,7 @@ if (input('Importar útimas medições? (S/N) ').upper() == ('S')) and (numDays 
         for date in dates:
             urlFull = url + date + complement
             res = requests.get(urlFull)
+            n = datetime.datetime.strptime(date, "%Y_%m_%d").date().strftime("%d/%m/%Y")
 
             if res.ok:
                 res.encoding = 'utf8'
@@ -85,9 +95,6 @@ if (input('Importar útimas medições? (S/N) ').upper() == ('S')) and (numDays 
                 # maior.
                 text = max(text, key=len)
                 text = text.find_all('tr')
-
-                # i = 0
-                n = datetime.datetime.strptime(date, "%Y_%m_%d").date().strftime("%d/%m/%Y")
 
                 for value in range(1, len(text) - 1):
                     linha = text[value].get_text().strip().replace(' \n\n', ';').replace('\n', ';')
@@ -105,5 +112,3 @@ if (input('Importar útimas medições? (S/N) ').upper() == ('S')) and (numDays 
 
 else:
     print('Nenhum dado importado')
-    # tratar o cabeçalho
-    # talvez importar os valores do find_all('tr')
